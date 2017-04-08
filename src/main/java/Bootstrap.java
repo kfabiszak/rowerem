02b4649aec762d13b6761fccf636651ddff0e540
M3tiii <m1995g@gmail.com>
2017-04-09 00:01:11 +0200

    Refactored post and get method.

--- src/main/java/Bootstrap.java
+++ src/main/java/Bootstrap.java
@@ -1,18 +1,64 @@
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import com.google.gson.Gson;
 
 import static spark.Spark.*;
 
-public class Bootstrap {
+class RouteModel {
+    private String origin;
+    private String destination;
+}
 
+class LoginModel {
+    private String login;
+    private String password;
+}
+
+class JsonTransformer {
+
+    public static String toJson(Object object) {
+        return new Gson().toJson(object);
+    }
+
+    public static <T extends Object> T  fromJson(String json, Class<T> classe) {
+        return new Gson().fromJson(json, classe);
+    }
+
+}
+
+/*
+    Dokumentacja: http://sparkjava.com/
+    Obsługa spark + MongoDb: https://blog.openshift.com/developing-single-page-web-applications-using-java-8-spark-mongodb-and-angularjs/
+
+    Odbieranie parametrów:
+     request.queryParams("param_name")
+    Odbieranie wiadomości:
+     Model_name element = jsonTransformer.fromJson(request.body(), Model_name.class);
+
+    #TODO
+    Klient przysyła informacje o trasie -> punkt startowy, docelowy, dodatkowe parametry
+    Serwer znajduje optymalną trasę w NextBike Api
+    Serwer wyznacza trasę w Google Map Api
+    Serwer zwraca gotową trasę do klienta
+
+    #TODO
+    Obsługa logowania rejestracji z MongoDb
+*/
+public class Bootstrap {
     public static void main(String[] args) {
-        get("/hello", new Route() {
-            @Override
-            public Object handle(Request request, Response response) {
-                System.out.println(request.params("adupa"));
-                return "Hello World!!";
-            }
+        JsonTransformer jsonTransformer = new JsonTransformer();
+
+        get("/test-get", (request, response) -> {
+            return "MSG TO CLIENT"; //Transfer object toJson
         });
+
+        post("/route", (request, response) -> {
+            RouteModel element = jsonTransformer.fromJson(request.body(), RouteModel.class);
+            return element;
+        });
+
+        post("/login", (request, response) -> {
+            LoginModel element = jsonTransformer.fromJson(request.body(), LoginModel.class);
+            return element;
+        });
+
     }
 }
